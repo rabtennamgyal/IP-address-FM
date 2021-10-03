fdc39fef42579c116b8624ab5b3ae025b9fc64e3
Rabten <rabten@protonmail.com>
2021-10-02 23:31:24 -0400

    created the leaflet map

--- src/js/components/head.js
+++ src/js/components/head.js
@@ -13,7 +13,38 @@ function Head() {
             </form>
 
             <div className='card'>
-                
+                <div className='content'>
+                    <h3>
+                        IP ADDRESS
+                    </h3>
+                    <p>
+                        112.233.222.121
+                    </p>
+                </div>
+                <div className='content'>
+                    <h3>
+                        LOCATION
+                    </h3>
+                    <p>
+                        Queens, NY 11377
+                    </p>
+                </div>
+                <div className='content'>
+                    <h3>
+                        TIMEZONE
+                    </h3>
+                    <p>
+                        UTC-500
+                    </p>
+                </div>
+                <div className='content'>
+                    <h3>
+                        ISP
+                    </h3>
+                    <p>
+                        SpaceX, Starlink
+                    </p>
+                </div>
             </div>
         </div>
     )
